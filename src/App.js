33cd2440b7253672677986d86cb6fb1a09c710f2
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-05-03 10:19:59 -0300

    Initial commit of my components without tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
+import "./app.scss";
+
+import Header from "./component/header";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return <Header />;
 }
 
 export default App;
---
+++ src/component/header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.scss";
+// import Logo from './../../assets'
+
+const Header = (props) => {
+  return (
+    <header>
+      <div className="wrap">
+        <div className="logo">Logo Image</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
